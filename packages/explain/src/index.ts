881e203fdecf14269bca6bc11a32fd3883a2aa60
Om Patel <YOUR_EMAIL_HERE>
2025-12-27 12:23:34 -0500

    feat(api): fix explain risk exports

--- packages/explain/src/index.ts
+++ packages/explain/src/index.ts
@@ -1,10 +1,10 @@
-export * from "./tree.js";
-
 export * from "./explain-margin-tree.js";
 export * from "./explain-contribution-tree.js";
-
-export * from "./explain-risk-tree.js";
-export * from "./explain-risk-delta-tree.js";
-
 export * from "./explain-skipped-changes-tree.js";
+
+export { attachConcentrationRiskToTrees } from "./explain-risk-tree.js";
+export { attachConcentrationDeltaToTrees } from "./explain-risk-delta-tree.js";
+
+export type { ExplainTree } from "./tree.js";
+
 
